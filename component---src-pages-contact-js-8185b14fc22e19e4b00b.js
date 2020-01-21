e29b8767edab831c6c6fd278f50be89ec0233848
kenniaa <kenniaa@users.noreply.github.com>
2020-01-21 00:33:07 +0000

    deployed via Gatsby Publish Action 🎩 for c1c589209461e7f00b7cb3d669eca8dd95acc938

---
+++ component---src-pages-contact-js-8185b14fc22e19e4b00b.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{"Cuy+":function(e,n,a){"use strict";a.r(n);var t=a("q1tI"),r=a.n(t),l=a("Bl7J"),c=a("vrFN");n.default=function(){return r.a.createElement(l.a,null,r.a.createElement(c.a,{title:"Work"}),r.a.createElement("h3",null,"[woman sleep]"),r.a.createElement("p",null,"NOOOOO"),r.a.createElement("a",{href:"https://github.com/kenniaa",target:"_blank",rel:"noopener noreferrer",className:"fancy-link"},"github"))}}}]);
+//# sourceMappingURL=component---src-pages-contact-js-8185b14fc22e19e4b00b.js.map
