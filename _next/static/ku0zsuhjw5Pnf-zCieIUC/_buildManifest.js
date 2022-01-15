7f622bbcc2f94bd382e8f90d536b0ea928f0949b
kenniaa <kennniaa@gmail.com>
2022-01-15 03:58:29 +0000

    Deploying to master from  @ 83d54d2c91f9c238c1852221a4999d66988315bf 🚀

---
+++ _next/static/ku0zsuhjw5Pnf-zCieIUC/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-a378142e74a7e0f58101.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-cac834d7a2d13af3ea09.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
