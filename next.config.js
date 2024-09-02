f80958071f0b80b3ceb3fe7fca3d5926a80c84fc
Kenia <kennniaa@gmail.com>
2024-09-02 16:56:58 -0400

    Update build process

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 const isGHPages = process.env.NODE_ENV === 'pages';
 
+/**
+ * @type {import('next').NextConfig}
+ */
 module.exports = {
+    output: 'export',
     assetPrefix: isGHPages ? '/' : './',
     compiler: {
         styledComponents: true,
-    }
+    },
 };
